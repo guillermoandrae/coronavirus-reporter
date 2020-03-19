3daed60a29211d42427cf2a8088c0a908989b0ed
guillermoandrae <guillermo@joinhandshake.com>
2020-03-19 10:49:05 -0400

    Refactoring tests using a dataProvider.

--- tests/ReporterTest.php
+++ tests/ReporterTest.php
@@ -2,26 +2,61 @@
 
 namespace GuillermoandraeTest\Coronavirus;
 
-use Guillermoandrae\Coronavirus\Contracts\AbstractSource;
 use Guillermoandrae\Coronavirus\Reporter;
 use Guillermoandrae\Coronavirus\SourceAggregator;
+use Guillermoandrae\Coronavirus\Sources\GeorgiaDepartmentOfHealth;
+use Guillermoandrae\Coronavirus\Sources\NewYorkDepartmentOfHealth;
+use Guillermoandrae\Coronavirus\Sources\PennsylvaniaDepartmentOfHealth;
 use PHPUnit\Framework\TestCase;
 
 final class ReporterTest extends TestCase
 {
-    public function testExecute()
+    /**
+     * @param string $className The source class name.
+     * @param string $state The state.
+     * @param string $path The path to the fixture data.
+     * @param string $numConfirmedCases The number of cases.
+     * @param string $date The date.
+     * @dataProvider getSources
+     */
+    public function testExecute(string $className, string $state, string $path, string $numConfirmedCases, string $date)
     {
         ob_start();
         $aggregator = new SourceAggregator();
-        $mock = $this->getMockForAbstractClass(
-            AbstractSource::class,
-            ['California'],
-            'CaliforniaDepartmentOfHealth'
-        );
-        $aggregator->addSource($mock);
+        $source = new $className('', $path);
+        $aggregator->addSource($source);
         $reporter = new Reporter($aggregator);
         $reporter->execute();
         $output = ob_get_clean();
-        $this->assertStringContainsString('California', $output);
+        $this->assertStringContainsString($state, $output);
+        $this->assertStringContainsString($numConfirmedCases, $output);
+        $this->assertStringContainsString($date, $output);
+    }
+
+    public function getSources(): array
+    {
+        return [
+            [
+                GeorgiaDepartmentOfHealth::class,
+                'Georgia',
+                'tests/Fixtures/ga.html',
+                '404',
+                'March 17, 2020 at 11:34 AM'
+            ],
+            [
+                NewYorkDepartmentOfHealth::class,
+                'New York',
+                'tests/Fixtures/ny.html',
+                '7,180',
+                'March 17, 2020 at 2:03 PM'
+            ],
+            [
+                PennsylvaniaDepartmentOfHealth::class,
+                'Pennsylvania',
+                'tests/Fixtures/pa.html',
+                '412',
+                'March 17, 2020 at 12:45 PM'
+            ],
+        ];
     }
 }
