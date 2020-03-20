fe1fd4a16ca93c9023532a16c8e0fdae93f8080c
guillermoandrae <guillermo@joinhandshake.com>
2020-03-20 14:15:47 -0400

    Resolved #1.

--- bin/api.php
+++ bin/api.php
@@ -2,6 +2,10 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+use Aws\S3\S3Client;
+use League\Flysystem\Filesystem;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use Cache\Adapter\Filesystem\FilesystemCachePool;
 use Guillermoandrae\Coronavirus\Api;
 use Guillermoandrae\Coronavirus\SourceAggregator;
 use Guillermoandrae\Coronavirus\Sources\NewYorkDepartmentOfHealth;
@@ -10,7 +14,15 @@ use Guillermoandrae\Coronavirus\Sources\GeorgiaDepartmentOfHealth;
 use Guillermoandrae\Coronavirus\Sources\CaliforniaDepartmentOfHealth;
 
 lambda(function (array $event) {
-    $aggregator = new SourceAggregator();
+    $s3client = new S3Client([
+        'region' => 'us-east-1',
+        'version' => 'latest'
+    ]);
+    $s3Adapter = new AwsS3Adapter($s3client, 'coronavirus-reporter');
+    $filesystem = new Filesystem($s3Adapter);
+    $cacheItemPool = new FilesystemCachePool($filesystem);
+
+    $aggregator = new SourceAggregator($cacheItemPool);
     $aggregator->addSource(new CaliforniaDepartmentOfHealth());
     $aggregator->addSource(new GeorgiaDepartmentOfHealth());
     $aggregator->addSource(new NewYorkDepartmentOfHealth());
